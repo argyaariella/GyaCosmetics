dd29712d13b07f873232ee460dd64f4729186522
Fikri Ramadhan <ajafikri999@gmail.com>
2026-06-17 08:55:42 +0700

    Delete gya-cosmetics directory

--- gya-cosmetics/get_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'config/config.php';
-$res = $conn->query("SHOW TABLES");
-while($row = $res->fetch_array()) echo $row[0] . "\n";
-echo "\n";
-$res = $conn->query("DESCRIBE users");
-while($row = $res->fetch_assoc()) echo json_encode($row) . "\n";
-?>
--- gya-cosmetics/update_db.php
+++
@@ -1,6 +0,0 @@
-<?php
-require 'config/config.php';
-$conn->query("ALTER TABLE transaksi ADD COLUMN promo_id INT(11) NULL AFTER user_id");
-$conn->query("ALTER TABLE transaksi ADD COLUMN diskon DECIMAL(15,2) DEFAULT '0.00' AFTER total_harga");
-echo "DB Updated!";
-?>
